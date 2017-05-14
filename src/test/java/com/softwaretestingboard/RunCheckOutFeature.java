b5fecc2df422c75e257ce49abd9500f99790a1e6
Mayur Shah <mail@mayurshah.in>
2017-05-14 17:40:03 +0530

    Fixed feature file.

--- src/test/java/com/softwaretestingboard/RunCheckOutFeature.java
+++ src/test/java/com/softwaretestingboard/RunCheckOutFeature.java
@@ -7,5 +7,5 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class) 
 @CucumberOptions(features = "src/test/resources/com/softwaretestingboard/checkout.feature",format = {"pretty", "html:target/cucumber","json:target/stock.json" })
-public class RunCheckOutFeature  {
+public class  RunCheckOutFeature  {
 }
